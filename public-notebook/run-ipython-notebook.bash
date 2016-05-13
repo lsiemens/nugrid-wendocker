3161bf1c770a9553b2cd43cafeb3724a51a14688
Luke Siemens <lsiemens@users.noreply.github.com>
2016-05-13 12:55:08 -0700

    checkout notebooks and startup.ipy from https://github.com/NuGrid/WENDI

--- public-notebook/run-ipython-notebook.bash
+++ public-notebook/run-ipython-notebook.bash
@@ -1,8 +1,10 @@
 #!/bin/bash                                                                     
 export PYTHONPATH=/home/nugrid
 
-IPN_URL="https://github.com/swjones/nugridnotebooks.git"
+IPN_URL="https://github.com/NuGrid/WENDI.git"
+#IPN_URL="https://github.com/swjones/nugridnotebooks.git"
 #IPN_URL="https://github.com/fherwig/nugridnotebooks.git"
+IP_PROFILE="/home/nugrid/.ipython/profile_nbserver"
 IPN_LOCAL="/home/nugrid/nugridnotebooks"
 IPN_DIR="${IPN_LOCAL}/notebooks"
 
@@ -11,6 +13,9 @@ OMEGA_SYGMA_DIR="/home/nugrid/omega_sygma"
 
 [[ -d ${IPN_DIR} ]] || git clone --depth 1 ${IPN_URL} ${IPN_LOCAL}
 [[ -d ${OMEGA_SYGMA_DIR} ]] || git clone --depth 1 ${OMEGA_SYGMA_URL} ${OMEGA_SYGMA_DIR}
+
+# move startup
+mv ${IPN_LOCAL}/startup/* ${IP_STARTUP}/startup/
 
 # add Luke's widget module to python path:
 export PYTHONPATH=${IPN_LOCAL}/modules:${OMEGA_SYGMA_DIR}:$PYTHONPATH
@@ -29,4 +34,3 @@ ipython notebook \
     --ip=0.0.0.0 \
     --port=8888 \
     --notebook-dir=${IPN_DIR}
-
